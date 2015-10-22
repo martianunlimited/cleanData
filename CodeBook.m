d8a45a045f8c54bbd574770ba5ba1f4173a5b939
martianunlimited <me@nicklim.com>
2015-10-22 18:47:25 +1300

    Create CodeBook.m

---
+++ CodeBook.m
@@ -0,0 +1,37 @@
+# Code book
+The variables are taken from features.txt and these are the description according to features_info.txt
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+These signals were used to estimate variables of the feature vector for each pattern: 
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions, suffix -mean() is used to denote the mean values, and suffix -std() is used to denote the standard deviation of the measurement:
+
+* tBodyAcc-XYZ
+* tGravityAcc-XYZ
+* tBodyAccJerk-XYZ
+* tBodyGyro-XYZ
+* tBodyGyroJerk-XYZ
+* tBodyAccMag
+* tGravityAccMag
+* tBodyAccJerkMag
+* tBodyGyroMag
+* tBodyGyroJerkMag
+* fBodyAcc-XYZ
+* fBodyAccJerk-XYZ
+* fBodyGyro-XYZ
+* fBodyAccMag
+* fBodyAccJerkMag
+* fBodyGyroMag
+* fBodyGyroJerkMag
+
+## Transformation done to the data 
+
+We load the data and responses, and subject from both the training data (x_train.txt, y_train.txt, subject_train.txt) 
+and the test data (x_test.txt, y_test.txt, subject_test.txt) respectively.
+We then merge the training and the test datasets into a single data frame, before extracting only the relevant features, 
+(namely the average (mean()) and the standard deviation (std()))
+
+We then aggregate the observations by activity (from y_train/y_test) and subject (from subject_train/subject_test) and aggregate 
+the observations by averaging the values. Each row in new dataset therefore represents the average value of each of the 
+features for the 30 subjects for 6 different activities (laying, sitting, standing, walking, walking_upstairs, walking_downstairs) 
